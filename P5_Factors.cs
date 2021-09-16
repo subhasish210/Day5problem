38c70cf4e52066b14c38dfe743b9b4dd328f7116
subhasish210 <subhasishsahu75@gmail.com>
2021-09-16 11:11:19 +0530

    second commit

--- P5_Factors.cs
+++ P5_Factors.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace Day5
 {
-    class P5_Factors
+    class P5_facts
     {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the prime factization for N: ");
+            long n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The prime factization of " + n + " is: ");
+
+            for (long fact = 2; fact * fact <= n; fact++)
+            {
+
+                while (n % fact == 0)
+                {
+                    Console.Write(fact + " ");
+                    n = n / fact;
+                }
+            }
+        }
     }
 }
